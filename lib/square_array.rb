6dff656035700f9abe4f4c4a96585187cc90e968
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-13 17:56:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   counter = 0 
     while counter < array.size do
-    new_array << (ary[counter] ** 2)
+    new_array << (array[counter] ** 2)
     counter += 1 
   end
   return new_array
